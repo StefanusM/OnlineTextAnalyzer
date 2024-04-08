18f287935ce7139406cbdcdc4e86b1b5df9d7d1d
Stefan Meusburger <stefan.meusburger@hotmail.com>
2024-04-08 20:59:24 +0200

    refactor

--- src/app/text-analyzer/service/text-analyzer.service.ts
+++ src/app/text-analyzer/service/text-analyzer.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const emptyVowelsCountMap = new Map<string, number>([
